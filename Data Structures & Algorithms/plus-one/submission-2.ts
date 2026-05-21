664ccaddd97b847bc6836030b65cc02413d1c174
Varun Hameer Dutia <51284086+varundutia@users.noreply.github.com>
2026-05-21 15:05:37 +0100

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} digits
+     * @return {number[]}
+     */
+    plusOne(digits: number[]): number[] {
+        const sum = digits[digits.length-1] + 1;
+        let carry = 0;
+        if(sum > 9){
+            digits[digits.length-1] = sum%10;
+            carry = 1;
+            for(let i=digits.length-2;i>=0;i--){
+                if(digits[i] + carry > 9){
+                    digits[i] = (digits[i]+carry)%10;
+                }
+                else{
+                    digits[i]++;
+                    carry = 0;
+                    break;
+                }
+            }
+            if(carry != 0){
+                return [carry, ...digits];
+            }
+            return digits;
+        }
+        digits[digits.length-1]++;
+        return digits;
+    }
+}
